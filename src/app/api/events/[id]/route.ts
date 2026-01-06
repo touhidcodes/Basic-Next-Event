0d1b44a82130540354512c99383f61b977de83e1
Touhidur Zaman <touhidcodes@gmail.com>
2026-01-06 18:17:54 +0600

    add: event apis

--- src/app/api/events/[id]/route.ts
+++ src/app/api/events/[id]/route.ts
@@ -26,3 +26,57 @@ export async function GET(
     );
   }
 }
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { db, client } = await mongoConnect();
+    const data = await req.json();
+
+    const result = await db
+      .collection("events")
+      .updateOne({ _id: new ObjectId(params.id) }, { $set: data });
+
+    // client.close();
+
+    if (result.matchedCount === 0) {
+      return NextResponse.json({ error: "Event not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: "Event updated" });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to update event" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { db, client } = await mongoConnect();
+
+    const result = await db
+      .collection("events")
+      .deleteOne({ _id: new ObjectId(params.id) });
+    // client.close();
+
+    if (result.deletedCount === 0) {
+      return NextResponse.json({ error: "Event not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: "Event deleted" });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to delete event" },
+      { status: 500 }
+    );
+  }
+}
--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,5 +1,6 @@
 import { mongoConnect } from "@/lib/mongoConnect";
-import { NextResponse } from "next/server";
+import { TEvent } from "@/types/event";
+import { NextRequest, NextResponse } from "next/server";
 
 // GET all events
 export async function GET() {
@@ -12,6 +13,40 @@ export async function GET() {
     console.error(error);
     return NextResponse.json(
       { error: "Failed to fetch events" },
+      { status: 500 }
+    );
+  }
+}
+
+// POST new event
+export async function POST(req: NextRequest) {
+  try {
+    const { db, client } = await mongoConnect();
+    const data: TEvent = await req.json();
+
+    // Basic validation
+    if (!data.title || !data.date || !data.location) {
+      client.close();
+      return NextResponse.json(
+        { error: "Title, date, and location required" },
+        { status: 400 }
+      );
+    }
+
+    const result = await db.collection("events").insertOne({
+      ...data,
+      createdAt: new Date(),
+    });
+
+    // client.close();
+    return NextResponse.json(
+      { message: "Event created", id: result.insertedId },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to create event" },
       { status: 500 }
     );
   }
